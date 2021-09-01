7a7d4f07179e2479eed28b7d2edb484f315706ac
rmdabunesar <abunesar13@gmail.com>
2021-09-01 18:53:20 +0600

    get_header get_footer

---
+++ footer.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Footer template file
+ * 
+ * @package Apex
+ */
+
+?>
+
+<footer>
+        Footer
+    </footer>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Main template file
+ * 
+ * @package Apex
+ */
+
+?>
+
+<?php get_header(); ?>
+    <div class="content">
+        Content
+    </div>
+<?php get_footer(); ?>
