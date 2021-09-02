7ccfb55b97b464c7d5143de9a9febd2f411595c6
rmdabunesar <abunesar13@gmail.com>
2021-09-02 18:58:11 +0600

    custom background

--- inc/classes/class-apex-theme.php
+++ inc/classes/class-apex-theme.php
@@ -25,12 +25,17 @@ class APEX_THEME {
 
     public function setup_theme() {
         add_theme_support( 'title-tag' );
+
         add_theme_support( 'custom-logo', [
             'header-text' => [ 'site-title', 'site-description' ],
             'height'      => 60,
             'width'       => 400,
             'flex-height' => true,
             'flex-width'  => true,
+        ] );
+
+        add_theme_support( 'custom-background', [
+            'default-color' => 'ffffff',
         ] );
     }
 }
