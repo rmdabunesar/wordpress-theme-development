30fddac727b0cf6901da6dd32703ee187015ef81
rmdabunesar <abunesar13@gmail.com>
2021-09-03 12:06:58 +0600

    wp nav menu

--- inc/classes/class-menus.php
+++ inc/classes/class-menus.php
@@ -29,4 +29,28 @@ class Menus {
             'apex-footer-menu' => esc_html__( 'Footer Menu', 'apex' ),
         ] );
     }
+
+    public function get_menu_id( $location ) {
+        // get all the location
+        $locations = get_nav_menu_locations();
+
+        // get object id by location
+        $menu_id = $locations[$location];
+
+        return ! empty( $menu_id ) ? $menu_id : '';
+    }
+
+    public function get_child_menu_items( $menu_array, $parent_id ) {
+        $child_menus = [];
+
+        if ( ! empty( $menu_array ) && is_array( $menu_array ) ) {
+            foreach ($menu_array as $menu) {
+                if ( intval( $menu->menu_item_parent ) === $parent_id ) {
+                    array_push( $child_menus, $menu );
+                }
+            }
+        }
+
+        return $child_menus;
+    }
 }
--- template-parts/header/nav.php
+++ template-parts/header/nav.php
@@ -5,10 +5,14 @@
  * @package Apex
  */
 
+$menu_class = \Apex_Theme\Inc\Menus::get_instance();
+$header_menu_id = $menu_class->get_menu_id ( 'apex-header-menu' );
+$header_menus = wp_get_nav_menu_items( $header_menu_id );
+
 ?>
 
 
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
+<nav class="navbar navbar-expand-lg" style="background-color: #e3f2fd;">
   <div class="container-fluid">
     <?php 
       if ( has_custom_logo() ) {
@@ -20,40 +24,59 @@
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Home</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Link</a>
-        </li>
-        <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Dropdown
-          </a>
-          <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-            <li><a class="dropdown-item" href="#">Action</a></li>
-            <li><a class="dropdown-item" href="#">Another action</a></li>
-            <li><hr class="dropdown-divider"></li>
-            <li><a class="dropdown-item" href="#">Something else here</a></li>
+    
+
+    <div class="collapse navbar-collapse" id="navbarNavDropdown">
+      <?php 
+        if ( ! empty( $header_menus ) && is_array( $header_menus ) ) {
+        ?>
+          <ul class="navbar-nav">
+            <?php 
+              foreach ($header_menus as $menu_item ) {
+                if ( ! $menu_item->menu_item_parent ) {
+                  $child_menu_item = $menu_class->get_child_menu_items( $header_menus, $menu_item->ID );
+                  $has_childern = ! empty( $child_menu_item ) && is_array( $child_menu_item );
+
+                  if ( ! $has_childern ) {
+                  ?>
+                    <li class="nav-item">
+                      <a class="nav-link" href="<?php echo esc_url( $menu_item->url ); ?>">
+                        <?php echo esc_html( $menu_item->title ); ?>
+                      </a>
+                    </li>
+                  <?php
+                  } else {
+                  ?>
+                    <li class="nav-item dropdown">
+                      <a class="nav-link dropdown-toggle" href="<?php echo esc_url( $menu_item->url ); ?>" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        <?php echo esc_html( $menu_item->title ); ?>
+                      </a>
+                      <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                      <?php 
+                        foreach ( $child_menu_items as $child_menu_item ) {
+                        ?>
+                          <a class="dropdown-item" href="<?php echo esc_url( $child_menu_item->url ); ?>">
+                            <?php echo esc_html( $child_menu_item->title ); ?>
+                          </a>
+                        <?php
+                        }
+                      ?>
+                      </div>
+                    </li>
+                  <?php
+                  }
+                ?>
+                <?php
+                }
+              }
+            ?>
           </ul>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
-        </li>
-      </ul>
-      <form class="d-flex">
-        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-        <button class="btn btn-outline-success" type="submit">Search</button>
-      </form>
+        <?php
+        }     
+      ?>
     </div>
+
+
   </div>
 </nav>
 
-<?php 
-  wp_nav_menu( array(
-    'theme_location'  => 'apex-header-menu',
-    'container_class' => 'my_extra_menu_class'
-  ) )
-?>
